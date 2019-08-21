926930c217a3e947e0c356602d968c066aca2b74
Sam <songsammy@users.noreply.github.com>
2019-08-21 15:50:48 +0800

    fix stupid mistakes

--- f.cpp
+++ f.cpp
@@ -1,5 +1,5 @@
 #include <cstdio>
-using naespace std;
+using namspace std;
 int main(){
 int n=6,k=3,m;
 int nu[n],ak[k];
@@ -9,7 +9,7 @@ while (ak[0]<n-k){
 for (int i=0;i<k;i++) printf("%d ",nu[ak[i]]);
 printf("\n");
 for (int j=k-1;j>=0;j--){
-if (ak[j]=n-k+j) continue;
+if (ak[j]==n-k+j) continue;
 else {
 ak[j]++;m=ak[j];
 for (int l=j;l<=k-1;l++) ak[l]=m+l-j;
