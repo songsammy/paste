d2722db809b27f1678cb2d92f408b03d301fd3e8
Sam <songsammy@users.noreply.github.com>
2019-08-21 16:00:17 +0800

    change m,q

--- f.cpp
+++ f.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 using namespace std;
 int main(){
-int n=6,k=3,m;
+int n=6,k=3,m,q;
 int nu[n],ak[k];
 for (int i=0;i<n;i++) nu[i]=i;
 for (int i=0;i<k;i++) ak[i]=i;
@@ -12,7 +12,7 @@ for (int j=k-1;j>=0;j--){
 if (ak[j]==n-k+j) continue;
 else {
 ak[j]++;m=ak[j];
-for (int l=j;l<=k-1;l++) ak[l]=m+l-j;
+for (int l=1;l+j<=k-1;l++) ak[l+j]=m+l;
 }
 }
 }
