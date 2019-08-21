fa117635cf9a82242e2edd647f1697f6c1c31111
Sam <songsammy@users.noreply.github.com>
2019-08-21 15:47:45 +0800

    Create f.cpp

---
+++ f.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+using naespace std;
+int main(){
+int n=6,k=3,m;
+int nu[n],ak[k];
+for (int i=0;i<n;i++) nu[i]=i;
+for (int i=0;i<k;i++) ak[i]=i;
+while (ak[0]<n-k){
+for (int i=0;i<k;i++) printf("%d ",nu[ak[i]]);
+printf("\n");
+for (int j=k-1;j>=0;j--){
+if (ak[j]=n-k+j) continue;
+else {
+ak[j]++;m=ak[j];
+for (int l=j;l<=k-1;l++) ak[l]=m+l-j;
+}
+}
+}
+}
