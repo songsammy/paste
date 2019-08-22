84bf8243b6f1387a1e939d2f502a69fb03a8f8c0
Sam <songsammy@users.noreply.github.com>
2019-08-22 09:35:44 +0800

    Create dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+using namespace std;
+int n=6,k=3,nu[3];
+void dfs(int used,int sta){
+nu[used]=sta;
+if (used==k-1) {
+for (int i=0;i<k;i++) printf("%d ",nu[i]);
+printf("\n");
+return ;
+}
+for (int i=sta+1;i<=n;i++){
+dfs(used+1,i);
+}
+return ;
+}
+int main(){
+//scanf("%d %d",&n,&k);
+dfs(0,1);
+return 0;
+}
